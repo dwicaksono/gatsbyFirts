a5e3efaec14620428f6a9c8ee3a80bf9de86a541
dwicaksono <novan.top@gmail.com>
2020-10-21 23:33:04 +0700

    add server with sanity db

---
+++ sanity/schemas/person.js
@@ -0,0 +1,37 @@
+import { MdPerson as icon } from 'react-icons/md'
+export default {
+    name:'person',
+    title: 'Slicemaster',
+    type:'document',
+    icon,
+    fields:[
+        {
+            name:'name',
+            title:'Name',
+            type:'string',
+        },
+        {
+            name:'slug',
+            title:'Slug',
+            type:'slug',
+            options:{
+                source:'name',
+                maxLength:100,
+            }
+        },
+        {
+            name:'description',
+            title:'Description',
+            type:'text',
+            description:'tell us a bit about this person'
+        },
+        {
+            name:'image',
+            title:'Image',
+            type:'image',
+            options:{
+                hotspot:true
+            }
+        }
+    ]    
+}
